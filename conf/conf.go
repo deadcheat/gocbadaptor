58807163f5f1bacbca782969a14fb2e8a2d93d7a
deadcheat <deadcheat@users.noreply.github.com>
2018-01-12 23:58:06 +0900

    add authenticate(not tested yet)

--- conf/conf.go
+++ conf/conf.go
@@ -4,15 +4,24 @@ import "github.com/couchbase/gocb"
 
 // Env struct for configure connection
 type Env struct {
-	ConnectString string `mapstructure:"connection"`
-	BucketName    string `mapstructure:"bucketname"`
-	Password      string `mapstructure:"password"`
-	CacheExpiry   uint32 `mapstructure:"expiry"`
+	ClusterUser     string `mapstructure:"clusteruser"`
+	ClusterPassword string `mapstructure:"clusterpassword"`
+	ConnectString   string `mapstructure:"connection"`
+	BucketName      string `mapstructure:"bucketname"`
+	Password        string `mapstructure:"password"`
+	CacheExpiry     uint32 `mapstructure:"expiry"`
 }
 
 // OpenBucket CouchBaseへの接続開始
 func (env *Env) OpenBucket() (bucket *gocb.Bucket, err error) {
-	cluster, err := gocb.Connect(env.ConnectString)
+	var cluster *gocb.Cluster
+	cluster, err = gocb.Connect(env.ConnectString)
+	if err == gocb.ErrAuthError {
+		err = cluster.Authenticate(gocb.PasswordAuthenticator{
+			Username: env.ClusterUser,
+			Password: env.ClusterPassword,
+		})
+	}
 	if err != nil {
 		return nil, err
 	}
